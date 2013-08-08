418bf9cc8bba498fe2212959b5ca0d7c03dff06d
jacook <jacob@jcook.cc>
2013-08-08 15:29:51 -0400

    generate keys if nonexistent

--- beacon/beacon.py
+++ beacon/beacon.py
@@ -65,12 +65,37 @@ def handle_client(sock):
 	elif data['request'] == 'ping':
 		sock.sendall(json.dumps({'response': 'ok'}))
 
+def gencert(sslcert, sslkey):
+	if not os.path.exists('/etc/beacon'):
+		os.mkdir('/etc/beacon')
+	k = OpenSSL.crypto.PKey()
+	k.generate_key(OpenSSL.crypto.TYPE_RSA, 1024)
+	crt = OpenSSL.crypto.X509()
+	crt.set_serial_number(1)
+	crt.gmtime_adj_notBefore(0)
+	crt.gmtime_adj_notAfter(10*365*24*60*60)
+	crt.set_pubkey(k)
+	crt.sign(k, 'sha1')
+	open(sslcert, "wt").write(
+		OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, crt))
+	open(sslkey, "wt").write(
+		OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, k))
+
 def serve_beacon():
 	sslctx = OpenSSL.SSL.Context(OpenSSL.SSL.SSLv3_METHOD)
 	sslcert = '/etc/beacon/cert.pem'
 	sslkey = '/etc/beacon/pkey.key'
-	sslctx.use_privatekey_file(sslkey)
-	sslctx.use_certificate_file(sslcert)
+
+	if not os.path.exists(sslcert) or not os.path.exists(sslkey):
+		gencert(sslcert, sslkey)
+
+	try:
+		sslctx.use_privatekey_file(sslkey)
+		sslctx.use_certificate_file(sslcert)
+	except:
+		gencert(sslcert, sslkey)
+		sslctx.use_privatekey_file(sslkey)
+		sslctx.use_certificate_file(sslcert)
 
 	s = socket.socket()
 	s = OpenSSL.SSL.Connection(sslctx, s)
